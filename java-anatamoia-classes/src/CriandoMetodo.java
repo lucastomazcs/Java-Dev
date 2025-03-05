6522ef153fbd06e00711d4056dd6f474691fb4ac
lucastomazcs <lucastomaz023@gmail.com>
2025-03-05 11:21:19 -0300

    testando

---
+++ java-anatamoia-classes/src/CriandoMetodo.java
@@ -0,0 +1,18 @@
+public class CriandoMetodo {
+
+    public static void main(String[] args) {
+        String primeiroNome = "Lucas";
+        String segundoNome = "Tomaz";
+
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+        
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+
+        return "Resultado do método:" + primeiroNome.concat(" ").concat(segundoNome);
+
+    }
+
+}
