b94e6b3970933e86b664d3b9384deba334e8a3d4
lucastomazcs <lucastomaz023@gmail.com>
2025-03-05 12:06:32 -0300

    TESTANDO

--- java-sintaxe/src/edu/lucas/variaveis/TiposVariaveis.java
+++ java-sintaxe/src/edu/lucas/variaveis/TiposVariaveis.java
@@ -2,4 +2,16 @@ package edu.lucas.variaveis;
 
 public class TiposVariaveis {
     
+    public static void main(String[] args) throws Exception{
+        
+        int numero = 2;
+
+        System.out.println(numero);
+
+        numero = 1;
+
+        System.out.println("O numero após a alteração:" + numero);
+
+    }
+
 }
